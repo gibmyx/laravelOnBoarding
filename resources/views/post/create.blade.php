5a558537724007eff7f7ba22d27336a907969708
buuu-u <victormarcella22@gmail.com>
2024-09-12 08:49:43 -0500

    Primer commit santiago

---
+++ resources/views/post/create.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel | Post | Create</title>
+</head>
+<body>
+
+<h1>Esta pagina creara los post</h1>
+
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class);
+
+Route::get('/post', [PostController::class, 'index']);
+
+Route::get('/post/create', [PostController::class, 'create']);
+
+Route::get('/post/{post}', [PostController::class, 'show']);
