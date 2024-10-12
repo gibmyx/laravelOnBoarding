e2a1d7200435dc5776ee383d2d3b782c56260224
samf2022 <samf0794@gmail.com>
2024-10-11 23:58:27 -0400

    ✅ Adding invoice creation test
    adding invoice creation test

---
+++ tests/Unit/CreateInvoiceTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+final class CreateInvoiceTest extends TestCase
+{
+    public function test_create_invoice(): void
+    {
+        $this->assertTrue(true);
+    }
+}
