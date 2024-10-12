48a38212d31d98ee0ca4b511e54658463ffd1ad5
samf2022 <samf0794@gmail.com>
2024-10-11 23:53:14 -0400

    ✏️ Fix typo
    fix typo

--- app/Interface/InvoiceItemRepositoryInterface.php
+++ app/Interface/InvoiceItemRepositoryInterface.php
@@ -7,5 +7,6 @@ use App\Entity\InvoiceItem;
 interface InvoiceItemRepositoryInterface
 {
     public function save(InvoiceItem $invoiceItem): void;
+    
     public function update(string $id, InvoiceItem $invoiceItem): void;
 }
