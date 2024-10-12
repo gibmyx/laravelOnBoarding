a7cd09eaee801b7cfb8eefc630b683ef678559de
samf2022 <samf0794@gmail.com>
2024-10-12 00:57:56 -0400

    ➕ Ramsey uuid dependencie
    ramsey uuid dependencie

--- tests/Unit/CreateInvoiceTest.php
+++ tests/Unit/CreateInvoiceTest.php
@@ -2,12 +2,30 @@
 
 namespace Tests\Unit;
 
+use App\DTO\InvoiceDTO;
+use App\Interface\InvoiceRepositoryInterface;
+use App\Models\Invoice;
+use App\UseCase\Invoice\CreateInvoice;
+use Mockery;
+use Mockery\Mock;
+use Ramsey\Uuid\Uuid;
 use Tests\TestCase;
 
 final class CreateInvoiceTest extends TestCase
 {
-    public function test_create_invoice(): void
+    public function test_create_invoice_without_items(): void
     {
-        $this->assertTrue(true);
+        $invoiceModel = new Invoice([
+            'id' => Uuid::uuid4()->toString(),
+            'code' => 'INV-1',
+            'status' => 'pending',
+            'provider_id' => '1',
+            'subtotal' => 20.5,
+            'tax' => 0,
+            'discount' => 0,
+            'total' => 20.5,
+        ]);
+
+        dd($invoiceModel);
     }
 }
